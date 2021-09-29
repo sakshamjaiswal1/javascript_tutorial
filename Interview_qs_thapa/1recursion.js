cfba10382465ca3626437b418f373fbd93d729f7
Saksham Jaiswal <sjais973@gmail.com>
2021-09-29 23:49:22 +0530

    vowel

--- Interview_qs_thapa/1recursion.js
+++ Interview_qs_thapa/1recursion.js
@@ -1,1 +1,14 @@
-console.log('recursion')
+// console.log('recursion')
+
+const num = 100;
+
+let x = 0;
+let y = 1;
+
+let fn = x+y
+while(fn<num){
+console.log(fn)
+fn= x+y
+x=y
+y=fn
+}
---
+++ Interview_qs_thapa/2const_or_vowel.js
@@ -0,0 +1,12 @@
+// console.log('vow')
+const findVowel=(i)=>{
+    if (i=='a' || i=='e' ||i== 'i'||i=='o'||i=='u'){
+        return 'it is a vowel'
+    }
+    else{
+        return ' it is a consonent'
+    }
+}
+
+console.log(findVowel('i'))
+console.log(findVowel('c'))
