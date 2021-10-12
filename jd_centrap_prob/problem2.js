744b2866ec0eb75228ea00a7cb5ce15014497ded
Saksham Jaiswal <sjais973@gmail.com>
2021-10-12 22:07:34 +0530

    new

--- jd_centrap_prob/problem2.js
+++ jd_centrap_prob/problem2.js
@@ -1,2 +1,6 @@
 console.log('problem2')
 
+const makeNewString=(str)=>
+str.length<3 ? str : str.slice(0,3)+str.slice(-3)
+
+// console.log(makeNewString('abc555def'))
--- js_int_prblms/script.js
+++ js_int_prblms/script.js
@@ -1,26 +1,55 @@
 console.log("qstn starts");
 
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-let arr2 = arr.reduce((total, i) => total + i, 0);
-console.log(arr2);
+// let arr2 = arr.reduce((total, i) => total + i, 0);
+// console.log(arr2);
 
-let people = [
-  {
-    name: "Sak",
-    age: 22,
-  },
-  { name: "tan", age: 22 },
-  {
-    name: "geets",
-    age: 24,
-  },
-];
+// let people = [
+//   {
+//     name: "Sak",
+//     age: 22,
+//   },
+//   { name: "tan", age: 22 },
+//   {
+//     name: "geets",
+//     age: 24,
+//   },
+// ];
 
-let pep = people.reduce((group,person)=>{
-    let age= person.age
-    if(group[age]==null) group[age]=[]
-    group[age].push(person.name)
-    return group
-},{})
-console.log(pep)
+// let pep = people.reduce((group,person)=>{
+//     let age= person.age
+//     if(group[age]==null) group[age]=[]
+//     group[age].push(person.name)
+//     return group
+// },{})
+// console.log(pep)
+
+// console.log("hello world");
+// console.log(this)
+// console.log(x)
+// var x=3
+
+// var x =3
+
+let main = document.getElementById('main')
+let btn = document.getElementById('btn')
+
+function handler (){
+    console.log('i am clicked')
+    btn.innerHTML='clicked'
+    btn.style.backgroundColor='green'
+    btn.style.padding='30px'
+  
+}
+
+
+// console.log(main)
+btn.addEventListener('click',()=>{
+    console.log('i am clicked')
+    main.innerHTML='saksham'
+})
+
+
+
+
