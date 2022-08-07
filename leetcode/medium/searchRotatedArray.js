ff0b8c52b2bd75115d7f3ce1fe915331a1699c6a
Saksham Jaiswal <sjais973@gmail.com>
2022-08-07 13:13:50 +0530

    new

---
+++ leetcode/medium/searchRotatedArray.js
@@ -0,0 +1,17 @@
+const searchArray= (nums,target)=>{
+let low = 0
+let high = nums.length-1
+let mid = Math.floor(low+high/2)
+while(low<=high){
+    if(nums[mid] ===target){
+        return mid
+    }
+if(nums[low]<=nums[mid]){
+    
+}
+
+    
+}
+}
+
+
