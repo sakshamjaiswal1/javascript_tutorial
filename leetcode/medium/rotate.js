c0da150d1c52b6d710a746582ec65ea64bf5dc91
sakshamjaiswal1 <sjais973@gmail.com>
2022-11-10 11:32:10 +0530

    rotate

--- leetcode/medium/rotate.js
+++ leetcode/medium/rotate.js
@@ -1,60 +1,74 @@
-const rotate=(matrix)=>{
-
-}
-
-const permute = function(nums) {
-    const result =[]
-    function traverse(nums,storeArr){
-        if(nums.length===0){
-            result.push([...storeArr])
-            return 
-        }
-        for(let num of nums){
-            storeArr.push(num)
-            const value = nums.filter(item=>item!==num)
-            traverse(value,storeArr)
-            storeArr.pop()
-        }
+const rotate = (matrix) => {
+  if (matrix === null || matrix.length === 0 || matrix[0].length === 0) {
+    return;
+  }
+  const rotateLayer = (matrix, start, end) => {
+    for (let i = 0; start + i < end; i++) {
+      let tempTop = matrix[start][start + i]; // save top
+      matrix[start][start + i] = matrix[end - i][start]; // left to top
+      matrix[end - i][start] = matrix[end][end - i]; // bottom to left
+      matrix[end][end - i] = matrix[start + i][end]; // right to bottom
+      matrix[start + i][end] = tempTop;
     }
-    traverse([...nums],[])
-    return result
+  };
+  let numberOfLayers = Math.floor(matrix.length / 2);
+  for (let i = 0; i < numberOfLayers; i++) {
+    rotateLayer(matrix, i, matrix.length - 1 - i);
+  }
 };
-var permutes = function(nums) {
-    const res = [];
-    const backtrack = (path) => {
-        if(path.length === nums.length) {
-            res.push(path);
-            return;
-        }
-        nums.forEach(n => {
-            if(path.includes(n)) { 
-                return; 
-            }
-            backtrack(path.concat(n))
-        })
+const permute = function (nums) {
+  const result = [];
+  function traverse(nums, storeArr) {
+    if (nums.length === 0) {
+      result.push([...storeArr]);
+      return;
     }
-    backtrack([]);
-    return res;
+    for (let num of nums) {
+      storeArr.push(num);
+      const value = nums.filter((item) => item !== num);
+      traverse(value, storeArr);
+      storeArr.pop();
+    }
+  }
+  traverse([...nums], []);
+  return result;
 };
-var permute2 = function(nums) {
-    var results = [];
-    var heapPermutation = function(nums, k) {
-        if(k == 1) {
-            results.push(nums.slice());
+var permutes = function (nums) {
+  const res = [];
+  const backtrack = (path) => {
+    if (path.length === nums.length) {
+      res.push(path);
+      return;
+    }
+    nums.forEach((n) => {
+      if (path.includes(n)) {
+        return;
+      }
+      backtrack(path.concat(n));
+    });
+  };
+  backtrack([]);
+  return res;
+};
+var permute2 = function (nums) {
+  var results = [];
+  var heapPermutation = function (nums, k) {
+    if (k == 1) {
+      results.push(nums.slice());
+    } else {
+      for (var i = 0; i < k; i++) {
+        heapPermutation(nums, k - 1);
+        var temp = nums[k - 1];
+        if (k % 2 == 0) {
+          nums[k - 1] = nums[i];
+          nums[i] = temp;
         } else {
-            for(var i=0; i<k; i++) {
-                heapPermutation(nums, k-1);
-                var temp = nums[k-1];
-                if(k%2 == 0) {
-                    nums[k-1] = nums[i];
-                    nums[i] = temp;
-                } else {
-                    nums[k-1] = nums[0];
-                    nums[0] = temp;
-                }
-            }
+          nums[k - 1] = nums[0];
+          nums[0] = temp;
         }
-        return results;
-    };
-    return heapPermutation(nums, nums.length);
+      }
+    }
+    return results;
+  };
+  return heapPermutation(nums, nums.length);
 };
