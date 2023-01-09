e7438f537fe5fb1beecafcd7d245ae7bc01bd1a4
sakshamjaiswal1 <sjais973@gmail.com>
2023-01-09 17:53:19 +0530

    unique-new

--- leetcode/medium/uniquePaths.js
+++ leetcode/medium/uniquePaths.js
@@ -10,4 +10,19 @@ const uniquePaths = (m, n) => {
   }
   return grid[m - 1][n - 1];
 };
-console.log(uniquePaths(3, 7));
+// console.log(uniquePaths(3, 7));
+
+const uniquePath2 = (m,n)=>{
+    let memo = new Array(m).fill(0).map(()=>new Array(n))
+    function paathsToPoint(row,col){
+        if(row===0||col===0) return 1
+        if(!memo[row][col]){
+            memo[row][col]=paathsToPoint(row-1,col) + paathsToPoint(row,col-1)
+        }
+        return memo[row][col]
+    }
+
+    return paathsToPoint(m-1,n-1)
+}
+
+console.log(uniquePath2(3,7))
