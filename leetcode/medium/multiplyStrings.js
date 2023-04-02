0d1c1639827caaf08267f0ad6ace6ec34201a5ce
sakshamjaiswal1 <sjais973@gmail.com>
2023-04-02 20:00:14 +0530

    string-multiply fixes #33

--- leetcode/medium/multiplyStrings.js
+++ leetcode/medium/multiplyStrings.js
@@ -1,6 +1,32 @@
-const multiply = (num1,num2)=>{
-if(num1==='0'|| num2==="0"){
-    return '0'
-}
-
-}
+const multiply = (num1, num2) => {
+  if (num1 === "0" || num2 === "0") {
+    return "0";
+  }
+  var products = [];
+  for (let i = num2.length - 1; i >= 0; i--) {
+    for (let j = num1.length - 1; j >= 0; j--) {
+      products[i + j + 1] =
+        products[i + j + 1] == undefined
+          ? parseInt(num1[j]) * parseInt(num2[i])
+          : products[i + j + 1] + parseInt(num1[j]) * parseInt(num2[i]);
+    }
+  }
+  var carryOver = 0;
+  for (let i = products.length - 1; i >= 0; i--) {
+    if (products[i] != undefined) {
+      let sum = Math.floor((products[i] + carryOver) % 10);
+      carryOver = Math.floor((products[i] + carryOver) / 10);
+      products[i] = sum;
+    } else {
+      products[i] = -1;
+    }
+  }
+  if (carryOver != 0) {
+    products[0] = carryOver;
+  }
+  let answer = "";
+  for (let i = 0; i < products.length; i++) {
+    answer = products[i] != -1 ? answer + products[i].toString() : answer + "";
+  }
+  return answer;
+};
--- leetcode/medium/swapPairs.js
+++ leetcode/medium/swapPairs.js
@@ -18,35 +18,18 @@ const swapPairs = (head) => {
     cur = temp;
     temp = cur.next.next;
   }
-  return ans
-};
-
-var swapPairsss = function (head) {
-  if (head === null) return null;
-  if (head.next === null) return head;
-
-  const ans = head.next;
-
-  let cur = head;
-  let temp = cur.next.next;
-
-  while (true) {
-    cur.next.next = cur;
-
-    if (temp === null) {
-      // even end
-      cur.next = null;
-      break;
-    } else if (temp.next === null) {
-      // odd end
-      cur.next = temp;
-      break;
-    }
-
-    cur.next = temp.next;
-    cur = temp;
-    temp = cur.next.next;
-  }
-
   return ans;
 };
+
+function setup() {
+  // Write your code here.
+  document.querySelectorAll(".remove").forEach((elem, index) => {
+    elem.addEventListener("click", () => {
+   
+      document.getElementsByClassName("image")[index].remove();
+    });
+  });
+  //   console.log(document.querySelectorAll(".remove"));
+}
+
+setup();
