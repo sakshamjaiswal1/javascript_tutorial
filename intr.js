7f6efb1812dd21e78163314b89ea040dfbf45078
sakshamjaiswal1 <sjais973@gmail.com>
2023-06-18 12:38:06 +0530

    extra-file-removed

--- intr.js
+++
@@ -1,7 +0,0 @@
-let x = "hiiiiiiii";
-
-// let y = x.split("").reverse().join("");
-
-let y = x.slice(0, -1);
-
-console.log(y);
--- test.js
+++
@@ -1,5 +0,0 @@
-let s = "saksham";
-let y = "sak";
-let z = s.substring(2);
-
-if (x.substring) console.log(z);
