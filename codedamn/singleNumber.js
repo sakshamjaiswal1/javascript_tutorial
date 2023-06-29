9065c3609702452e35f20952df0e4d9f00489d27
sakshamjaiswal1 <sjais973@gmail.com>
2023-06-29 17:38:33 +0530

    nextGreatestLetter

--- codedamn/singleNumber.js
+++ codedamn/singleNumber.js
@@ -73,3 +73,15 @@ const maxProfit = function (prices) {
   }
   return maxProfit
 }
+
+
+function nextGreatestLetter(letters, target) {
+    
+  for (let i=0;i<letters.length;i++){
+      if(letters[i]>target){
+          return letters[i]
+      }
+  }
+  return letters[0]
+  }
+  
