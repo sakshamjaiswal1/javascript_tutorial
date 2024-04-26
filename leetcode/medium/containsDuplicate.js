d61335f7059eb4430a61ece08188eac373badd89
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-04-26 11:38:49 +0530

    isValidBST

--- leetcode/medium/containsDuplicate.js
+++ leetcode/medium/containsDuplicate.js
@@ -25,3 +25,22 @@ const rotateRight = (head, k) => {
   tail.next = null;
   return head;
 };
+
+// unique path with obstacles
+
+const isValidBSTHelper = (node, min, max) => {
+  if (node === null) {
+    return true;
+  }
+  if (node.val <= min || node.val >= max) {
+    return false;
+  }
+  return (
+    isValidBSTHelper(node?.left, min, node?.val) &&
+    isValidBSTHelper(node?.right, node?.val, max)
+  );
+};
+
+const isValidBST = (root) => {
+  return isValidBSTHelper(root, -Infinity, Infinity);
+};
