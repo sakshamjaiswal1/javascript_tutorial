b93dc655b86913262b62edf57817bdbf2c1d9dc2
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-04-28 12:15:45 +0530

    dfs

--- leetcode/medium/containsDuplicate.js
+++ leetcode/medium/containsDuplicate.js
@@ -77,3 +77,34 @@ const maxDepth = (root) => {
   let right = maxDepth(root.right);
   return 1 + Math.max(left, right);
 };
+
+const maxDepthBFS = (root) => {
+  if (!root) return 0;
+  let depth = 0;
+  let queue = [root];
+  while (queue.length > 0) {
+    const levelSize = queue?.length;
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue?.shift();
+      if (node?.left) queue.push(node?.left);
+      if (node.right) queue.push(node.right);
+    }
+    depth++;
+  }
+  return depth;
+};
+
+const maxDepthDFSIterative = (root) => {
+  if (!root) {
+    return 0;
+  }
+  let stack = [{ node: root, level: 1 }];
+  let depth = 0;
+  while (stack.length > 0) {
+    const { node, level } = stack.pop();
+    depth = Math.max(depth, level);
+    if (node.left) stack.push({ node: node.left, level: level + 1 });
+    if (node.right) stack.push({ node: node?.right, level: level + 1 });
+  }
+  return depth;
+};
