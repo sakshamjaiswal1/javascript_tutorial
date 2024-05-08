90cc368e607dd176b8fa5492a21bc976211ec385
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-05-08 12:07:06 +0530

    corr

--- leetcode/medium/sortedArrayToBST.js
+++ leetcode/medium/sortedArrayToBST.js
@@ -281,28 +281,27 @@ const buildTrees = function (inorder, postorder) {
   return recur(0, inorder.length - 1);
 };
 const sortedListToBST = function (head) {
-  function getarr(head) {
-    if (!head) {
-      return [];
-    }
+ function getarr(head){
+    if(!head) return [];
     let temp = head;
-    let out = [];
-    while (temp) {
-      out.push(temp.val);
-      temp = temp.mext;
+    let out = []
+    while(temp){
+        out.push(temp.val);
+        temp = temp.next;
     }
     return out;
-  }
-  function tree(nums, start = 0, end = nums?.length - 1) {
-    if (start <= end) {
-      let mid = Math.floor((start + end) / 2);
-      let root = new TreeNode(nums[mid]);
-      root.left = tree(nums, start, mid - 1);
-      root.right = tree(nums, mid + 1, end);
-      return root;
+}
+function tree(nums, start=0, end = nums.length-1) {
+    if(start<=end){
+        let mid = Math.floor((start+end)/2);
+        // console.log(mid, end)
+        let root = new TreeNode(nums[mid]);
+        root.left = tree(nums, start, mid-1);
+        root.right = tree(nums, mid+1, end);
+        return root;
     }
     return null;
-  }
+};
   let arr = getarr(head);
   return tree(arr);
 };
