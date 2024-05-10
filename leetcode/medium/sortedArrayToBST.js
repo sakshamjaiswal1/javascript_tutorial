c8869bf6b07afb216b086c06a833859482c70f0d
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-05-10 12:38:31 +0530

    minDepth

--- leetcode/medium/sortedArrayToBST.js
+++ leetcode/medium/sortedArrayToBST.js
@@ -320,3 +320,19 @@ const isBalanced = function (root) {
     return diff < 2 && isBalanced(root.left) && isBalanced(root.right);
   }
 };
+
+const minDepth = function (root) {
+  if (root === null) {
+    return 0;
+  }
+  if (root.left === null && root.right === null) {
+    return 1;
+  }
+  if (root.left === null) {
+    return minDepth(root.right) + 1;
+  }
+  if (root.right === null) {
+    return minDepth(root.left) + 1;
+  }
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
