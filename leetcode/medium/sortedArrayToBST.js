368bc976785ce9a0319a54f478af6a5e9f6259dc
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-05-14 14:11:20 +0530

    connect

--- leetcode/medium/sortedArrayToBST.js
+++ leetcode/medium/sortedArrayToBST.js
@@ -404,3 +404,14 @@ const flatten = (root) => {
   };
   if (root) revPreOrder(root);
 };
+
+const connect = function (root) {
+  if (!root || !root.left) {
+    return root;
+  }
+  root.left.next = root.right;
+  root.right.next = root?.next ? root.next.left : null;
+  connect(root.left);
+  connect(root.right);
+  return root;
+};
