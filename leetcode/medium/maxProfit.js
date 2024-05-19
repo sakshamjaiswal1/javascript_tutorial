c4f0cae269e5b7f49c9caf7d79f4db22c79f590a
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-05-19 12:27:38 +0530

    largest-consecutive

--- leetcode/medium/maxProfit.js
+++ leetcode/medium/maxProfit.js
@@ -13,3 +13,27 @@ const maxProfit = function (prices) {
   }
   return output;
 };
+
+const largestConsecutive = function (nums) {
+  nums.sort((a, b) => a - b);
+  let count = 0;
+  let temp = 1;
+  if (nums.length === 0) {
+    return 0;
+  }
+  if (nums?.length === 1) {
+    return 1;
+  }
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1] - 1) {
+      temp++;
+      count = Math.max(count, temp);
+    } else if (nums[i] === nums[i + 1]) {
+      count = Math.max(count, temp);
+    } else {
+      temp = 1;
+      count = Math.max(count, temp);
+    }
+  }
+  return count;
+};
