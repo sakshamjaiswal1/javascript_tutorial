33961277efb04b5a146a8f505a07f60db230e20d
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-05-21 14:48:02 +0530

    parition

--- leetcode/medium/maxProfit.js
+++ leetcode/medium/maxProfit.js
@@ -55,3 +55,36 @@ const sumNumbers = function (root) {
   }
   return sum;
 };
+
+const partition = function (s) {
+  const isPalindrome = (sub) => {
+    let left = 0;
+    let right = sub?.length - 1;
+
+    while (left < right) {
+      if (sub[left++] !== sub[right--]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const backtrack = (start, path, partitions) => {
+    if (start === s.length) {
+      partitions.push([...path]);
+      return;
+    }
+    for (let end = start + 1; end <= s.length; end++) {
+      const sub = s.substring(start, end);
+      if (isPalindrome(sub)) {
+        path.push(sub);
+        backtrack(end, path, partitions);
+        path.pop();
+      }
+    }
+  };
+
+  const partitions = [];
+  backtrack(0, [], partitions);
+  return partitions;
+};
