71321c4295a5df242045838bb36f83873dec5763
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-05-25 13:18:25 +0530

    cur

--- leetcode/medium/maxProfit.js
+++ leetcode/medium/maxProfit.js
@@ -134,3 +134,20 @@ const singleNumber = function (nums) {
     }
   }
 };
+
+const copyRandomList = function (head) {
+  const hashMap = new Map();
+  let cur = head;
+  while (cur) {
+    hashMap.set(cur, new Node(cur.value));
+    cur = cur.next;
+  }
+  cur = head;
+  while (cur) {
+    const copy = hashMap.get(cur);
+    copy.next = hashMap.get(cur.next) || null;
+    copy.random = hashMap.get(cur.random) || null;
+    cur = cur.next;
+  }
+  return hashMap.get(head);
+};
