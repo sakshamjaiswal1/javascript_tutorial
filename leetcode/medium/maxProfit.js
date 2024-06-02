20e15bd8578e2717c4e352a432b4b7d4dee3bffb
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-06-02 18:37:55 +0530

    reversek group

--- leetcode/medium/maxProfit.js
+++ leetcode/medium/maxProfit.js
@@ -254,3 +254,25 @@ const sortList = function (head) {
   }
   return head;
 };
+
+const reverseKGroup = function (head, k) {
+  let stack = [];
+  let newNode = new ListNode(-1);
+  let temp = newNode;
+
+  while (head) {
+    for (let i = 0; i < k && head; i++) {
+      stack.push(head);
+      head = head.next;
+    }
+
+    if (stack.length === k) {
+      while (stack.length > 0) {
+        temp.next = stack.pop();
+        temp = temp.next;
+      }
+      temp.next = head;
+    }
+  }
+  return newNode.next;
+};
