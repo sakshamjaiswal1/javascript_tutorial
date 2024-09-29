d749b11ef89fa193e29497a1b212bd3f206f28b6
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2024-09-29 17:23:06 +0530

    maxporduct

--- leetcode/medium/maxProfit.js
+++ leetcode/medium/maxProfit.js
@@ -434,12 +434,12 @@ const maxProduct = function (nums) {
   let prevMax = nums[0];
   let prevMin = nums[0];
   let result = nums[0];
-  for (let i = 1; nums.length; i++) {
+  for (let i = 1; i < nums.length; i++) {
     curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
     curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
     prevMax = curMax;
     prevMin = curMin;
-    result = Math.max(curMax, resut);
+    result = Math.max(curMax, result);
   }
   return result;
 };
