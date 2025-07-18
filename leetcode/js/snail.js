3ddf3878786683fc64686d4d1900425acabbaf84
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2025-07-18 23:29:39 +0530

    deep-equal

--- leetcode/js/snail.js
+++ leetcode/js/snail.js
@@ -93,3 +93,33 @@ function deepClone(obj) {
     cloneObj[key] = deepClone(obj[key]);
   }
 }
+
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (
+    typeof obj1 !== "object" ||
+    typeof obj2 !== "object" ||
+    obj1 === null ||
+    obj2 === null
+  ) {
+    return false;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1?.length !== keys2?.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (!keys2?.includes(key)) {
+      return false;
+    }
+    if (!deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+  return true;
+}
